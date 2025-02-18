66dcde53beb22130fdf1bb296d32cece985ef0cc
Thechemicalmen <florian.vandemert@outlook.be>
2025-02-18 11:27:12 +0100

    Ajout du projet "Mediator"

---
+++ Mediator/Mediator.cs
@@ -0,0 +1,45 @@
+﻿namespace Mediator
+{
+    public interface ICommand { }
+    public record AcheterArticle(string Article) : ICommand;
+
+    public interface ICommandHandler<T> where T : ICommand
+    {
+        Task HandleAsync(T command);
+    }
+
+    public class AcheterArticleHandler : ICommandHandler<AcheterArticle>
+    {
+        public Task HandleAsync(AcheterArticle command)
+        {
+            Console.WriteLine($"Article acheté : {command.Article}");
+            return Task.CompletedTask;
+        }
+    }
+
+    public interface IMyMediator
+    {
+        void RegisterHandler<T>(ICommandHandler<T> handler) where T : ICommand;
+        Task Handle<T>(T command) where T : ICommand;
+    }
+
+    public class MyMediator : IMyMediator
+    {
+        private Dictionary<Type, List<object>> handlers = [];
+
+        public async Task Handle<T>(T command) where T : ICommand
+        {
+            if (handlers.TryGetValue(command.GetType(), out List<object>? localHandlers))
+                foreach(var handler in localHandlers)
+                    await ((ICommandHandler<T>)handler).HandleAsync(command);
+        }
+        public void RegisterHandler<T>(ICommandHandler<T> handler) where T : ICommand
+        {
+            var commandType = typeof(T);
+            if (handlers.TryGetValue(commandType, out List<object>? value))
+                value.Add(handler);
+            else
+                handlers.Add(commandType, [handler]);
+        }
+    }
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,7 @@
+﻿using Mediator;
+
+var mediator = new MyMediator();
+
+mediator.RegisterHandler(new AcheterArticleHandler());
+
+await mediator.Handle(new AcheterArticle("Marteau"));
