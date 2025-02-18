fb74d80e43062eecd8bb683759c2271eebe6c79d
Thechemicalmen <florian.vandemert@outlook.be>
2025-02-18 11:02:34 +0100

    Ajout du projet "Interpreter"

---
+++ Iterator/Iterator.cs
@@ -0,0 +1,51 @@
+﻿namespace Iterator
+{
+    public class Personne
+    {
+        public string Nom { get; set; }
+    }
+
+    public interface IPersonneAlphabetIterator
+    {
+        Personne Current { get; }
+        bool MoveNext();
+    }
+
+    public class PersonneAlphabetIterator : IPersonneAlphabetIterator
+    {
+        private List<Personne> _copy;
+        private int _current;
+
+        public PersonneAlphabetIterator(PersonneCollection collection)
+        {
+            _copy = collection.ToList();
+            _copy.Sort((p1, p2) => string.Compare(p1.Nom, p2.Nom));
+            _current = -1;
+        }
+
+        public Personne Current
+        {
+            get
+            {
+                if (_copy.Count > _current)
+                    return _copy[_current];
+                return default!;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (_copy.Count > _current + 1)
+            {
+                _current++;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class PersonneCollection : List<Personne>
+    {
+        public IPersonneAlphabetIterator GetIterator() => new PersonneAlphabetIterator(this);
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,15 @@
+﻿using Iterator;
+
+var p = new PersonneCollection()
+{
+    new Personne() { Nom = "Alice" },
+    new Personne() { Nom = "Charles" },
+    new Personne() { Nom = "Bob" },
+    new Personne() { Nom = "Adam" },
+    new Personne() { Nom = "Obiwan" }
+};
+
+var iterator = p.GetIterator();
+
+while (iterator.MoveNext())
+    Console.WriteLine(iterator.Current?.Nom);
