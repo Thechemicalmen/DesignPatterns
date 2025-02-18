caf3f4f229a1eece2fd2dd9c53883dd57184a607
Thechemicalmen <florian.vandemert@outlook.be>
2025-02-18 14:35:45 +0100

    Ajout du projet "Strategy"

---
+++ Strategy/Program.cs
@@ -0,0 +1,19 @@
+﻿using Strategy;
+
+var purchase1 = new Purchase
+{
+    Id = 1,
+    Amount = 10m
+};
+var purschase2 = new Purchase
+{
+    Id = 2,
+    Amount = 15m
+};
+
+var manager = new PurchaseManager([]);
+manager.Add(purchase1);
+manager.Add(purschase2);
+
+manager.Pay(new VisaPaymentManager(), 1);
+manager.Pay(new PaypalPaymentManager(), 2);
---
+++ Strategy/Purchase.cs
@@ -0,0 +1,46 @@
+﻿namespace Strategy
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public interface IPaymentManager
+    {
+        void Pay(Purchase purchase);
+    }
+
+    public class VisaPaymentManager : IPaymentManager
+    {
+        public void Pay(Purchase purchase) => Console.WriteLine("Paiement avec Visa");
+    }
+
+    public class PaypalPaymentManager : IPaymentManager
+    {
+        public void Pay(Purchase purchase) => Console.WriteLine("Paiement avec Paypal");
+    }
+
+    public enum PaymentMode
+    {
+        Visa,
+        Paypal
+    }
+
+    public class PurchaseManager(ICollection<IPaymentManager> paymentManagers)
+    {
+        private List<Purchase> _purchases = [];
+
+        public void Add(Purchase purchase)
+        {
+            _purchases.Add(purchase);
+        }
+
+        public void Pay(IPaymentManager paymentManager, int id)
+        {
+            var purchase = _purchases.Find(p => p.Id == id);
+            if (purchase is not null)
+                paymentManager.Pay(purchase);
+        }
+    }
+}
