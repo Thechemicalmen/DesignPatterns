1ae9bbcf94587cf8f221409695d88c1c09a999af
Florian Vande Mert <florian.vandemert@outlook.be>
2025-05-01 19:32:14 +0200

    Ajout du design pattern "Repository" (non-officiel)

---
+++ RepositoryPattern/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace RepositoryPattern
+{
+    public interface IProductRepository : IReadProductRepository, IWriteProductRepository
+    {
+    }
+
+    public interface IReadProductRepository
+    {
+        IReadOnlyList<Product> GetProductsInStock();
+    }
+
+    public interface IWriteProductRepository
+    {
+        void AddProductInStock(Product product);
+        void DeleteProductFromStock(Product product);
+    }
+}
---
+++ RepositoryPattern/InMemoryProductRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace RepositoryPattern
+{
+    public class InMemoryProductRepository : IProductRepository
+    {
+        public void AddProductInStock(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProductFromStock(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Product> GetProductsInStock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
